5dc48e551364218156111d136635f33fef4952c3
Colette <colette.rs.shaw@gmail.com>
2020-05-07 22:19:25 +0100

    Add assembleMessage()

--- src/main/java/com/notsparta/crss/App.java
+++ src/main/java/com/notsparta/crss/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SendEmail sendEmail = new SendEmail();
+        sendEmail.assembleMessage("Bob");
     }
 }
--- src/main/java/com/notsparta/crss/SendEmail.java
+++ src/main/java/com/notsparta/crss/SendEmail.java
@@ -1,4 +1,21 @@
 package com.notsparta.crss;
 
+import java.util.Random;
+
 public class SendEmail {
+    public String assembleMessage(String name) {
+        String message = "";
+        String[] adjectives = {"happy", "joyful", "cheerful", "fortunate", "lucky"};
+        Random random = new Random();
+        int i = random.nextInt(adjectives.length);
+        message += name + "! This is a " + adjectives[i] + " test.";
+        System.out.println(message);
+        return message;
+    }
+    public void openNewEmailWindow() {
+
+    }
+    public void sendEmail(String emailAddress) {
+        emailAddress = EmailAddressContainer.emailAddress;
+    }
 }
