cb32cdcdde80d0bb060e512449e5f92445480f83
Colette Shaw <cshaw@spartaglobal.com>
2020-05-09 14:21:22 +0100

    Add sentence to message

--- src/main/java/com/notsparta/crss/SendEmail.java
+++ src/main/java/com/notsparta/crss/SendEmail.java
@@ -8,13 +8,20 @@ public class SendEmail {
         String[] greetings = {"greetings", "hello", "hi", "hey"};
         String[] adjectives = {"happy", "joyful", "cheerful", "fortunate", "lucky"};
         String[] timePeriod = {"day", "week", "month", "year"};
+        String[] goals = {"motivate yourself", "get inspired", "cheer up", "feel better", "stop crying", "fix your sleeping pattern", "repair your ruined relationships"};
+        String[] transitiveVerbs = {"write", "lend", "bake", "play", "buy", "donate", "read", "send", "sing", "teach", "get", "make", "take", "steal"};
+        String[] nouns = {"cat", "child", "book", "song", "cake", "truck", "car", "bed", "pillow", "sock", "pineapple"};
         Random random = new Random();
         int adjectivesIndex = random.nextInt(adjectives.length);
         int greetingsIndex = random.nextInt(greetings.length);
         int timePeriodIndex = random.nextInt(timePeriod.length);
+        int goalsIndex = random.nextInt(goals.length);
+        int nounsIndex = random.nextInt(nouns.length);
+        int transitiveVerbsIndex = random.nextInt(transitiveVerbs.length);
         String greeting = greetings[greetingsIndex].substring(0, 1).toUpperCase() + greetings[greetingsIndex].substring(1, greetings[greetingsIndex].length()).toLowerCase() + ", " + name + "!";
         String openingSentence = "I hope you are having a " + adjectives[adjectivesIndex] + " " + timePeriod[timePeriodIndex] + ".";
-        message += greeting + "\n\n" + openingSentence;
+        String content = "";
+        message += greeting + "\n\n" + openingSentence + "\n\nTo " + goals[goalsIndex] + ", " + transitiveVerbs[transitiveVerbsIndex] + " a " + nouns[nounsIndex] + "!";
         System.out.println(message);
         return message;
     }
