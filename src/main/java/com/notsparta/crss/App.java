28d74b610f7e1d4d9fc340b4ed9441f944f8c38f
Colette Shaw <cshaw@spartaglobal.com>
2020-05-09 15:52:24 +0100

    Set up Selenium to open Gmail window

--- src/main/java/com/notsparta/crss/App.java
+++ src/main/java/com/notsparta/crss/App.java
@@ -1,14 +1,20 @@
 package com.notsparta.crss;
 
-/**
- * Hello world!
- *
- */
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class App 
 {
     public static void main( String[] args )
     {
         SendEmail sendEmail = new SendEmail();
         sendEmail.assembleMessage("Bob");
+
+        setup();
+    }
+
+    public static void setup() {
+        WebDriver webDriver = new ChromeDriver();
+        new GmailPage(webDriver);
     }
 }
--- src/main/java/com/notsparta/crss/GmailPage.java
+++ src/main/java/com/notsparta/crss/GmailPage.java
@@ -1,4 +1,17 @@
 package com.notsparta.crss;
 
+import org.openqa.selenium.WebDriver;
+
 public class GmailPage {
+    WebDriver webDriver;
+
+    public GmailPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        setUp(webDriver);
+        webDriver.get("https://mail.google.com/");
+    }
+
+    public void setUp(WebDriver webDriver) {
+        webDriver.manage().window().maximize();
+    }
 }
